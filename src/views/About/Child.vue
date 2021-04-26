2b869888c3d06ec429b161783e3d00aa6713b164
Hikaru Takemoto <h.take1320@gmail.com>
2021-04-26 21:30:56 +0900

    :art: #7 router動作確認用のコード追加 (#8)

---
+++ src/views/About/Child.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="AboutChild">
+    <h1>Child!!</h1>
+
+    <div class="todos">
+      <template v-for="sortTodo in sortTodos" :key="sortTodo">
+        <p @click="removeTodo(sortTodo)">{{ sortTodo }}</p>
+      </template>
+    </div>
+
+    <div>
+      <input type="text" v-model="state.inputValue" />
+      <button @click="addTodo">hoge</button>
+      <p>{{ state.inputValue }}</p>
+      <div v-if="state.isError" style="background: red">error desuyo</div>
+    </div>
+    <div>
+      <input type="text" v-model="notReactive" />
+      <p>{{ notReactive }}</p>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, reactive, computed, watchEffect } from 'vue';
+
+interface State {
+  todos: string[];
+  inputValue: string;
+  isError: boolean;
+}
+
+export default defineComponent({
+  setup() {
+    const state = reactive<State>({
+      todos: [],
+      inputValue: 'hoge',
+      isError: false,
+    });
+
+    const notReactive = 'hgoe';
+
+    const sortTodos = computed(() => {
+      return [...state.todos].sort();
+    });
+
+    const addTodo = () => {
+      state.todos.push(state.inputValue);
+    };
+
+    const removeTodo = (value: string) => {
+      state.todos = state.todos.filter((v) => v !== value);
+    };
+
+    watchEffect(() => {
+      state.isError = state.inputValue === 'error';
+    });
+
+    return {
+      state,
+      sortTodos,
+      addTodo,
+      removeTodo,
+      notReactive,
+    };
+  },
+});
+</script>
