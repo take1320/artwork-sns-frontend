e44e35704fc3e1a28e8f6cf79083b0229173da4f
Hikaru Takemoto <h.take1320@gmail.com>
2021-04-26 21:29:40 +0900

    :art: #7 router動作確認用のコード追加

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
 import Home from '../views/Home.vue';
+import About from '../views/About/index.vue';
+import AboutChild from '../views/About/Child.vue';
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -10,11 +12,14 @@ const routes: Array<RouteRecordRaw> = [
   {
     path: '/about',
     name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    component: About,
+    children: [
+      {
+        path: 'child',
+        name: 'AboutChild',
+        component: AboutChild,
+      },
+    ],
   },
 ];
 
---
+++ src/views/About/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="about">
+    <h1>This is an about page index</h1>
+    <a href="/about/child">here!!</a>
+    <RouterView></RouterView>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { RouterView } from 'vue-router';
+
+export default defineComponent({
+  components: { RouterView },
+});
+</script>
